1d3fc9ee210119136a9677ddaf3c76d8a80bb76e
keshav0098 <93341651+keshav0098@users.noreply.github.com>
2021-11-15 13:00:38 +0530

    Update Controller.java
    Add Test line

--- src/main/java/com/keshav/microservice/Controller.java
+++ src/main/java/com/keshav/microservice/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//Git pull
 public class Controller {
 
 	@GetMapping("/Name")
